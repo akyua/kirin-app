55d35048aeefc2073b926fd3c24b8e5ab5c133d9
akyua <gabrielnovatoo@gmail.com>
2023-11-07 20:51:05 -0300

    header semi-completed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,2 +1,95 @@
 <template>
+    <header>
+        <nav id="logo">
+            <a href="/">
+                <img src="/kirin2.png" alt="logo kirin">
+            </a>
+        </nav>
+        <nav class="topics">
+            <ul class="menu">
+                <li><a href="#">Animes</a></li>
+                <li><a href="#">Ranking</a></li>
+                <li><a href="#">Reviews</a></li>
+                <li>
+                    <a href="/login">
+                        <div id="user-icon">
+                            <img src="../assets/user.png" alt="user" class="user-icon">
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+@import '../variables/variables.scss';
+
+    header{
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        padding-top: 20px;
+        box-shadow: inset 0px 20px 10px -10px rgba(0, 0, 0, 0.5);
+    }
+    #logo a img{
+        width: 100px;
+    }
+
+    .topics, .menu, #logo{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .topics ul{
+        display: flex;
+        gap: 40px;
+        list-style-type: none;
+        
+    }
+    .topics ul li a{
+        text-decoration: none;
+        font-size: $font-topics;
+        color: rgb(255, 255, 255);
+        font-weight: 600;
+        text-shadow: 3px 3px 5px rgba(0, 0, 0, 0.8);
+    }
+
+    .menu a {
+        text-decoration: none;
+        position: relative;
+        overflow: hidden;
+        margin-bottom: 10px;
+    }
+    .menu a span {
+        position: relative;
+        display: inline-block;
+        }
+
+    
+    .menu a::before {
+        content: "";
+        position: absolute;
+        bottom: -3px;
+        left: 50%;
+        transform: translateX(-50%);
+        width: 0;
+        height: 4px;
+        background-color: red; /* Cor da barra de destaque */
+        border-radius: 20px;
+        transition: width 0.4s; /* Duração da animação de crescimento */
+    }
+
+    .menu a:hover::before {
+        width: 100%; /* A barra crescerá até 100% da largura do link */
+    }
+
+    .user-icon{
+        width: 60px;
+        height: 60px;
+        display: flex;
+    }
+</style>
